7ba56a7ea93de615e602636d9c32062ab421c997
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2023-02-13 17:12:56 +0500

    БД
    Поправил структуру git, удалил лишнее. Поставил заглушку gitignore. Переписал модель

--- fridge/product_list/models.py
+++ fridge/product_list/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=12, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    birth_date = models.DateField(blank=True)
+    registration_date = models.DateField(auto_now_add=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.SmallIntegerField()
+    date_purchase = models.DateField(auto_now_add=True)
+    type = models.ForeignKey('Type', on_delete=models.PROTECT)
+    user = models.ForeignKey('User', on_delete=models.PROTECT)
+    Spoil = models.DateTimeField()
+
+class Type(models.Model):
+    name = models.CharField(max_length=100)
+    Expiration_date = models.SmallIntegerField()
