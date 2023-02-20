baf0d1c0fd2f050ae39399f78be44486ad06a92e
Sergey <59055584+Sergey-1221@users.noreply.github.com>
2023-02-21 00:53:47 +0300

    Update

--- fridge/product_list/views.py
+++ fridge/product_list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
+#from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+def show_list(request):
+    products = Product.objects.filter(user=request.user)
+    return render(request, 'show_list.html', {"products": products})
